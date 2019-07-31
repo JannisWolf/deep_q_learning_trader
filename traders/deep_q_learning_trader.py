61f03c2838800f8f3f9df98f40086074d663e75b
Jannis Wolf <jannis.wolf@fau.de>
2019-07-31 13:40:00 +0200

    final trader and network

--- traders/deep_q_learning_trader.py
+++ traders/deep_q_learning_trader.py
@@ -20,12 +20,32 @@ from framework.logger import logger
 from framework.stock_data import StockData
 from framework.stock_market_data import StockMarketData
 
-# assure stable results
-from numpy.random import seed
-seed(42)
-from tensorflow import set_random_seed
-set_random_seed(69)
+# Assure stable results
+# Seed value
+# Apparently you may use different seed values at each stage
+seed_value = 42
 
+# 1. Set `PYTHONHASHSEED` environment variable at a fixed value
+import os
+os.environ['PYTHONHASHSEED'] = str(seed_value)
+
+# 2. Set `python` built-in pseudo-random generator at a fixed value
+import random
+random.seed(seed_value)
+
+# 3. Set `numpy` pseudo-random generator at a fixed value
+import numpy as np
+np.random.seed(seed_value)
+
+# 4. Set `tensorflow` pseudo-random generator at a fixed value
+import tensorflow as tf
+tf.set_random_seed(seed_value)
+
+# 5. Configure a new global `tensorflow` session
+from keras import backend as K
+session_conf = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
+sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
+K.set_session(sess)
 
 
 
@@ -36,7 +56,7 @@ class DeepQLearningTrader(ITrader):
     RELATIVE_DATA_DIRECTORY = 'traders/dql_trader_data'
 
     def __init__(self, expert_a: IExpert, expert_b: IExpert, load_trained_model: bool = True,
-                 train_while_trading: bool = False, color: str = 'black', name: str = 'dql_trader', ):
+                 train_while_trading: bool = False, color: str = 'black', name: str = 'dql_trader',):
         """
         Constructor
         Args:
@@ -52,9 +72,26 @@ class DeepQLearningTrader(ITrader):
         self.expert_b = expert_b
         self.train_while_trading = train_while_trading
 
+        # Comment the action space you dont want to use
+        '''
+        self.actions = [[Vote.BUY, Vote.BUY],
+                        [Vote.BUY, Vote.SELL],
+                        [Vote.BUY, Vote.HOLD],
+                        [Vote.SELL, Vote.BUY],
+                        [Vote.SELL, Vote.SELL],
+                        [Vote.SELL, Vote.HOLD],
+                        [Vote.HOLD, Vote.BUY],
+                        [Vote.HOLD, Vote.SELL],
+                        [Vote.HOLD, Vote.HOLD]]
+        '''
+        self.actions = [[Vote.BUY, Vote.BUY],
+                        [Vote.BUY, Vote.SELL],
+                        [Vote.SELL, Vote.BUY],
+                        [Vote.SELL, Vote.SELL]]
+
         # Parameters for neural network
         self.state_size = 2
-        self.action_size = 4  # 10
+        self.action_size = len(self.actions)  # 10
         self.hidden_size = 50
 
         # Parameters for deep Q-learning
@@ -89,21 +126,9 @@ class DeepQLearningTrader(ITrader):
         self.model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
         # self.model.summary()
         self.vote2num = {Vote.BUY: 1, Vote.SELL: 2, Vote.HOLD: 3}
-        #self.actions = [[Vote.BUY, Vote.BUY],
-        #                [Vote.BUY, Vote.SELL],
-         #               [Vote.BUY, Vote.HOLD],
-         #               [Vote.SELL, Vote.BUY],
-          #              [Vote.SELL, Vote.SELL],
-           #             [Vote.SELL, Vote.HOLD],
-            #            [Vote.HOLD, Vote.BUY],
-             #           [Vote.HOLD, Vote.SELL],
-              #          [Vote.HOLD, Vote.HOLD]]
-        self.actions = [[Vote.BUY, Vote.BUY],
-                        [Vote.BUY, Vote.SELL],
-                        [Vote.SELL, Vote.BUY],
-                        [Vote.SELL, Vote.SELL]]
+        self.gamma = 0
 
-    def save_trained_model(self, epsiode):
+    def save_trained_model(self):
         """
         Save the trained neural network under a fixed name specific for this traders.
         """
@@ -113,11 +138,20 @@ class DeepQLearningTrader(ITrader):
     def _remember(self, state, action, reward, next_state):
         self.memory.append((state, action, reward, next_state))
 
-    def _act(self, state, stock_market_data, portfolio, order_list):
+    def _act(self, state, stock_market_data, portfolio, order_list) -> int:
+        """
+        Helper function that implements the traders next action. Creates todays order_list and returns performed action.
+
+        :param state: List
+        :param stock_market_data:
+        :param portfolio: Portfolio
+        :param order_list: List[Order]
+        :return: action: int
+        """
+
         if self.train_while_trading:
-            #if False:
             if np.random.rand() <= self.epsilon:
-                #print('random action performed, epsilon = ' + str(self.epsilon))
+                print('random action performed, epsilon = ' + str(self.epsilon))
                 action = random.randrange(self.action_size)
             else:
                 try:
@@ -153,44 +187,55 @@ class DeepQLearningTrader(ITrader):
         return action
 
     def _replay(self):
+        """
+        Helper function that implements experience replay and epsilon greedy policy
+
+        :return: None
+        """
+
         minibatch = random.sample(self.memory, self.batch_size)
-        #print('learn')
-        # for state, action, reward, next_state in minibatch:
-        #     target = reward
-        #     if next_state.ndim == 1:
-        #         next_state = np.array([next_state])
-        #     #target = reward + self.learning_rate * \
-        #     #         np.amax(self.model.predict(next_state)[0])
-        #
-        #     if state.ndim == 1:
-        #         state = np.array([state])
-        #     target_f = self.model.predict(state, batch_size=self.batch_size)
-        #     target_f[0][action] = target
-        #     self.model.fit(state, target_f, epochs=1, verbose=0)
         state = np.array(minibatch)[:,0]
         action = np.array(minibatch)[:,1]
         reward = np.array(minibatch)[:,2]
         next_state = np.array(minibatch)[:,3]
 
-        #if next_state.ndim == 1:
-        #    next_state = np.array([next_state])
-        #for idx, rew in enumerate(reward):
-        #    reward[idx] = rew + 0.1 * \
-        #         np.amax(self.model.predict(np.array([next_state[idx]])))
+        next_state_processed = np.array([])
+        next_state = [np.append(next_state_processed, st) for st in next_state]
+        next_state = np.array(next_state)
+
+        # next line only needed when self.gamma > 0
+        #next_reward = self.model.predict(next_state, batch_size=self.batch_size)
+
         state_processed = np.array([])
         state = [np.append(state_processed, st) for st in state]
         state = np.array(state)
-        if state.ndim == 1:
-            state = np.array([state])
+
         target_f = self.model.predict(state, batch_size=self.batch_size)
         for idx, tar in enumerate(target_f):
             tar[action[idx]] = reward[idx]
+
+            # comment next line in to use self.gamma > 0
+            # tar[action[idx]] = reward[idx] + 0.9 * max(next_reward[idx])
+
         self.model.fit(state, target_f, epochs=1, verbose=0, batch_size=self.batch_size)
+        
+        # epsilon greedy policy implementation here
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
     def __follow_action(self, company: Company, stock_data: StockData, vote: Vote, portfolio: Portfolio,
-                             order_list: List[Order]):
+                        order_list: List[Order]):
+        """
+        Protected helper method to calculate amount of stocks to be bought and sold.
+
+        :param company: Company
+        :param stock_data: StockData
+        :param vote: Vote
+        :param portfolio: Portfolio
+        :param order_list: List[Order]
+        :return: None (writes result to order_list)
+        """
+
         assert company is not None
         assert stock_data is not None
         assert vote is not None
@@ -226,69 +271,54 @@ class DeepQLearningTrader(ITrader):
         Returns:
           A OrderList instance, may be empty never None
         """
+
         assert portfolio is not None
         assert stock_market_data is not None
         assert stock_market_data.get_companies() == [Company.A, Company.B]
 
-        # TODO Compute the current state
-        #print(stock_market_data[Company.A].get_dates()[-1])
-
         order_list = []
-
         vote_a = self.expert_a.vote(stock_market_data[Company.A])
         vote_b = self.expert_b.vote(stock_market_data[Company.B])
 
         # convert votes to state
         state = np.array([self.vote2num[vote_a], self.vote2num[vote_b]])
 
-        # TODO Store state as experience (memory) and train the neural network only if trade() was called before at
-        #  least once
+        if self.train_while_trading:
+            if len(self.memory) > self.min_size_of_memory_before_training:
+                # helper function which executes experience replay
+                self._replay()
 
-        # TODO Create actions for current state and decrease epsilon for fewer random actions
-
-        # TODO Save created state, actions and portfolio value for the next call of trade()
-
-        # Decide action
+        # act
         action = self._act(state, stock_market_data, portfolio, order_list)
-        # Advance the game to the next frame based on the action.
-        # Reward is 1 for every frame the pole survived
-        #next_state, reward, done, _ = env.step(action)
-        #next_state = np.reshape(next_state, [1, 4])
-        # Remember the previous state, action, reward, and done
+
         if self.last_portfolio_value is not None:
+            # Reward function R1
             if self.last_portfolio_value <= portfolio.get_value(stock_market_data):
-                #reward = 2 - portfolio.get_value(stock_market_data) / self.last_portfolio_value
                 reward = 1
             else:
                 reward = 0
+
+            # Reward function R2
+            #reward = (portfolio.get_value(
+            #        stock_market_data) - self.last_portfolio_value) / self.last_portfolio_value
+
+            # helper to fill up the queue for performance replay
             self._remember(self.last_state, action, reward, state)
+
+        # save state and portfolio value
         self.last_portfolio_value = portfolio.get_value(stock_market_data)
         self.last_state = state
 
-        # make next_state the new current state for the next frame.
-        #state = next_state
-        # done becomes True when the game ends
-        # ex) The agent drops the pole
-        #if done:
-            # print the score and break out of the loop
-        #    print("episode: {}/{}, score: {}"
-        #         .format(e, episodes, time_t))
-        #    break
-        # train the agent with the experience of the episode
-        if self.train_while_trading:
-            if len(self.memory) > self.min_size_of_memory_before_training:
-                self._replay()
         return order_list
 
 
 
 # This method retrains the traders from scratch using training data from TRAINING and test data from TESTING
-EPISODES = 15
+EPISODES = 1
 if __name__ == "__main__":
     # Create the training data and testing data
     # Hint: You can crop the training data with training_data.deepcopy_first_n_items(n)
     training_data = StockMarketData([Company.A, Company.B], [Period.TRAINING])
-    #a=training_data.deepcopy_first_n_items(1002)
     testing_data = StockMarketData([Company.A, Company.B], [Period.TESTING])
 
     # Create the stock exchange and one traders to train the net
@@ -304,7 +334,7 @@ if __name__ == "__main__":
 
         # train the net
         stock_exchange.run(training_data, [training_trader])
-        training_trader.save_trained_model(i)
+        training_trader.save_trained_model()
         final_values_training.append(stock_exchange.get_final_portfolio_value(training_trader))
 
         # test the trained net
@@ -321,16 +351,16 @@ if __name__ == "__main__":
     plt.figure()
     plt.subplot(211)
     plt.plot(final_values_training, label='training', color="black")
-    plt.plot(final_values_test, label='test', color="green")
-    plt.title('final portfolio value training vs. final portfolio value test')
+    #plt.plot(final_values_test, label='test', color="green")
+    plt.title('final portfolio value training')
     plt.ylabel('final portfolio value')
     plt.xlabel('episode')
-    plt.legend(['training', 'test'])
+    plt.legend(['training'])
 
 
     plt.subplot(212)
     plt.plot(final_values_test, label='test', color="green")
-    plt.title('final portfolio value training vs. final portfolio value test')
+    plt.title('final portfolio value test')
     plt.ylabel('final portfolio value')
     plt.xlabel('episode')
     plt.legend(['test'])
